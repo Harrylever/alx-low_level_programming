0dd244825a7bd70e8adc59be95ec4bed865c392f
Harrylever <ukanah15thdean@gmail.com>
2022-09-08 18:54:31 -0700

    Task 101 is done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 /**
- * main - Entry point
- *
- * Description: 'Return an Error'
- *
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  * Return: Always 1 (Error)
  */
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	write(2, str, strlen(str));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
