1639d4637ebda50e1047179c814dbbfee3dddd16
Harrylever <ukanah15thdean@gmail.com>
2022-09-13 12:59:00 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Print out putchar and a new line'
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
