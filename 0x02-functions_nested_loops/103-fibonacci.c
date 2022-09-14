5579ac3f57cf71759ab047521c3eb6a1e4e6123d
Harrylever <ukanah15thdean@gmail.com>
2022-09-14 08:56:05 -0700

    Task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,18 +7,23 @@
 
 int main(void)
 {
-	int i, new_result, x = 0, y = 1;
+	int i, next;
+	long int new_result, x = 0, y = 1;
 
 	for (i = 1; i <= 50; i++)
 	{
 		new_result = x + y;
 
-		if (new_result % 2 == 0)
+		if (new_result < 4000000)
 		{
-			printf("%d\n", new_result);
+			if (new_result % 2 == 0)
+			{
+				next += new_result;
+			}
 		}
 		x = y;
 		y = new_result;
 	}
+	printf("%d\n", next);
 	return (0);
 }
