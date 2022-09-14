3e4004d9c5d5ae7aa54b7b1363d7079790fdd52f
Harrylever <ukanah15thdean@gmail.com>
2022-09-14 09:00:05 -0700

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-	int i, new_result, x = 0, y = 1;
+	int i;
+	long int new_result, x = 0, y = 1;
 
 	for (i = 1; i <= 98; i++)
 	{
 		new_result = x + y;
-		printf("%d,\n", new_result);
+		printf("%ld,\n", new_result);
 		x = y;
 		y = new_result;
 	}
