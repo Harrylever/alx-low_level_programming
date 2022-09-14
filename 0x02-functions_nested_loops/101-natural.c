f58fcec0b3d1c84fcfabbf01674df45f0d265dd3
Harrylever <ukanah15thdean@gmail.com>
2022-09-14 07:39:18 -0700

    Task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i, result;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			result += i;
+		}
+	}
+	printf("%d\n", result);
+}
