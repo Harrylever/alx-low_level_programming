d0d2eb979966ce2bc8e6cafb948229aa12ba790e
Harrylever <76135767+Harrylever@users.noreply.github.com>
2022-09-16 12:39:26 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,15 +12,20 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if (year % 4 == 0 || year % 400 == 0)
 	{
-		if (!(year % 100 == 0) && !(year == 4000))
+		if (!(year % 100 == 0) && !(year == 4000) && !(year == 2000))
 		{
 			if (month >= 2 && day >= 60)
 			{
 				day++;
 			}
 
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
+		else if ((year % 100 == 0) && (year == 2000))
+		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
