e537a489dc5faac643ed1232cc90c708a12d60e0
Harrylever <ukanah15thdean@gmail.com>
2022-09-19 20:43:28 -0700

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,14 +21,14 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			printf(str[i]);
+			printf("%c", str[i]);
 		}
 	} else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-			printf(str[n + 1]);
+			printf("%c", str[n + 1]);
 		}
 	}
-	printf('\n');
+	printf("\n");
 }
