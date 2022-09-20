cf6ab306c506c84dd79390b0c83d546825626094
Harrylever <ukanah15thdean@gmail.com>
2022-09-19 20:32:45 -0700

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,26 +4,31 @@
  * puts_half - prints half of a string
  * followed by a new line
  * @str: string to be printed
- * Return: void
  */
 
 void puts_half(char *str)
 {
-	int n, i;
+	int len, n, i;
 
-	i = 0;
+	len = 0;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
 	{
-		i++;
+		len++;
 	}
 
-	n = (i + 1) / 2;
-
-	for (i = n; str[i]; i++)
+	if (len % 2 == 0)
 	{
-		printf(str[i]);
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			printf(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			printf(str[n + 1]);
+		}
 	}
 	printf('\n');
 }
-
