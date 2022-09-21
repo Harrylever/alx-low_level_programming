3775e4037164d481062f9be806358fee8c9a66e6
Harrylever <ukanah15thdean@gmail.com>
2022-09-21 15:40:03 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,23 +5,23 @@
  * of integers
  * @a: an array of integers
  * @n: the number of elements to swap
- * Return: reversed array 
+ * Return: reversed array
  */
 
 void reverse_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    i = n - 1;
-    while (i >= 0)
-    {
-        printf("%d", a[i]);
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        i--;
-    }
-    printf("\n");
+	i = n - 1;
+	while (i >= 0)
+	{
+		printf("%d", a[i]);
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		i--;
+	}
+	printf("\n");
 }
 
