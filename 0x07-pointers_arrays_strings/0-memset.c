62092b48a2fbb0593ed7d8895651cd59e0933fe9
Harrylever <ukanah15thdean@gmail.com>
2022-09-26 12:31:44 -0700

    Task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_memset - Function to fill a block of memory with a particular value
+ * @ptr: any variable
+ * @x: Value to be filled
+ * @n: Number of bytes to be filled starting from ptr to be filled
+ * Return: void
+ */
+
+void *_memset(char *ptr, char x, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		ptr[i] = x;
+	}
+
+	return (ptr);
+}
+
